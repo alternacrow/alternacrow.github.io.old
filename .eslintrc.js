30a9310c9d96365629bd1e82c398b59e7cce7fe9
kazuaki21 <k.horita21+github@gmail.com>
2020-04-07 00:08:21 +0900

    setup develop environment

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "google",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "max-len": null,
+    "require-jsdoc": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+  },
+};

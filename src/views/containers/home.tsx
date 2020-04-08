7a5d1e390c519bb500d6eafab43d23f6e3510391
kazuaki21 <k.horita21+github@gmail.com>
2020-04-08 21:02:31 +0900

    test deploy

--- src/views/containers/home.tsx
+++ src/views/containers/home.tsx
@@ -1,4 +1,5 @@
 import React, {FC} from 'react';
+import styled from 'styled-components';
 
 import {useUser} from '../../states/user';
 
@@ -7,7 +8,37 @@ const HomeContainer: FC = () => {
 
   if (error) return <h1>Error</h1>;
 
-  return <div>{JSON.stringify(user)}</div>;
+  if (!user) return <h1>Loading</h1>;
+
+  return (
+    <Container>
+      <FadeInText>{JSON.stringify(user)}</FadeInText>
+    </Container>
+  );
 };
 
 export default HomeContainer;
+
+const Container = styled.div`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FadeInText = styled.p`
+  font-size: 20px;
+  font-weight: bold;
+  animation-name: fadein;
+  animation-duration: 1s;
+  animation-iteration-count: 1;
+  @keyframes fadein {
+    from {
+      opacity: 0;
+      transform: translateY(12px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`;

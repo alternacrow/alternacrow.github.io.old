efd6035885187ee245c1523ddc16281adbb5bdf5
kazuaki21 <k.horita21+github@gmail.com>
2020-09-14 21:28:20 +0900

    add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  processors: ['stylelint-processor-styled-components'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-styled-components',
+    'stylelint-prettier/recommended',
+  ],
+};
